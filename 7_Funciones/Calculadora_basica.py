0c5ff00e2b1c38c6d557775a123f8317cd1b4ccf
Zeus9141 <148802387+Zeus9141@users.noreply.github.com>
2024-05-31 10:22:36 -0600

    Actualizacion

---
+++ 7_Funciones/Calculadora_basica.py
@@ -0,0 +1,52 @@
+opcion=True
+while opcion1:
+print("\n\t..:::Calculadora Basica:::...\n 1.-Suma \n 2.-Resta \n 3.-Multiplicasion \n 4.- Division \n 5.- Sañir")
+opcion=input("\t Elige una opcion: ") .upper()
+
+
+if opcion=="1" or opcion=="+" or opcion=="SUMA":
+    n1=int(input{"Numero #1: "})
+    n2=int(input{"Numero #2: "})
+    SUMA=n1+n2
+    print(f"(n1)+(n2)=(suma)")
+
+
+if opcion=="2" or opcion=="-" or opcion=="RESTA":
+    n1=int (input{"Numero #1: "})
+    n2=int (input{"Numero #2: "})
+    RESTA=n1-n2
+    print(f"(n1)-(n2)=(Resta)")
+
+
+if opcion=="3" or opcion=="*" or opcion=="MULTIPLICASION":
+    n1=int(input{"Numero #1: "})
+    n2=int(input{"Numero #2: "})
+    MULTIPLICASION=n1*n2
+    print(f"(n1)*(n2)=(MULTIPLICASION)")
+
+
+if opcion=="4" or opcion=="/" or opcion=="DIVISION":
+    n1=int(input{"Numero #1: "})
+    n2=int(input{"Numero #2: "})
+    DIVISION=n1//n2
+    print(f"(n1)//(n2)=(DIVISION)")
+else:
+    print("Terminaste la ejecusion del SW")
+opcion1=False
+
+def=solicitarNUmeros():
+    n1=int(input ("Numero #1: ")
+    n2=int(input ("Numero #2: ")
+
+def operacionAritmetica(num1,num2,ope):
+    if opcion=="1" or opcion=="+" or opcion=="SUMA":
+    return f"{n1}+{n2}={n1+n2}"
+    elif opcion=="2" or opcion=="-" or opcion=="RESTA":
+    return f"{n1}-{n2}={n1-n2}"
+    elif opcion=="3" or opcion=="*" or opcion=="MULTIPLICASION":
+    return f"{n1}*{n2}={n1*n2}"
+    elif opcion=="4" or opcion=="/" or opcion=="RESTA":
+    return f"{n1}-{n2}={n1-n2}"
+
+solicitarnumeros()
+printoperacionAritmetica(n1,n2,operacion)
